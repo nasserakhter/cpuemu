b0240253a6cbc10ec34f201708ae7ccfcc2e7fb5
Nasser Akhter <nasrj3@gmail.com>
2023-05-04 20:50:11 -0700

    pusfhsodufda

--- drivers/2.bios.asm
+++ drivers/2.bios.asm
@@ -2,8 +2,19 @@ start:
   JMP main
 
 handler:
+  POP R5    ; get the return address
+  PUSH R5   ; push it back on the stack
+  CALL findFreeMemoryLocation
   MOV R6, 0x42494f53 ; Store the string "BIOS" in R6
   HLT
+
+findFreeMemoryLocation:
+  MOV R0, 0 ; Start at address 0
+  JZ found
+  INC R0    ; Increment the address
+  MOV R1, [R0] ; Get the value at the address
+found:
+  RET
 
 main:
   ADR handler   ; Get the address of the handler subroutine
